8571aa5dec7517fdf688237b3b66d61b1d847734
Drdla <drdla@gmail.com>
2024-11-27 20:40:20 +0100

    Add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ionicmovieapp',
+  appName: 'ionicmovieapp',
+  webDir: 'www'
+};
+
+export default config;
