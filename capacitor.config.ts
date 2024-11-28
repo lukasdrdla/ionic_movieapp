3d6de2f185187a286b5c4752e833e5771c8297f9
Drdla <drdla@gmail.com>
2024-11-28 16:47:46 +0100

    Add icon and splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,19 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ionicmovieapp',
   appName: 'ionicmovieapp',
-  webDir: 'www'
+  webDir: 'www',
+
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: "#f44033",
+      showSpinner: false,
+      androidSpinnerStyle: "small",
+      iosSpinnerStyle: "small",
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
