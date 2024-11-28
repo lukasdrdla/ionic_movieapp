cf5873f6cf86161d1f482a98480e683cf00d64fa
Drdla <drdla@gmail.com>
2024-11-28 16:58:04 +0100

    Change app id and name, better UI

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.ionicmovieapp',
-  appName: 'ionicmovieapp',
+  appId: 'com.cinemio.app',
+  appName: 'Cinemio',
   webDir: 'www',
 
   plugins: {
